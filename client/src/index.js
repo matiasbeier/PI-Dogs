423e5a6763b58a222541e2840b87b3693d763c28
matiasbeier <matiasbeier1@gmail.com>
2022-03-14 16:26:52 -0300

    working in deploy vercel

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,8 @@ import store from './store';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom'
 import axios from 'axios';
-import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
 
